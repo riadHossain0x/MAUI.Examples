431e416965bb4ef51643e7274bae03568abf8e0f
Riad <riadofficial.me@gmail.com>
2024-05-02 16:47:55 +0600

    text align

--- BasicApp/MainPage.xaml.cs
+++ BasicApp/MainPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class MainPage : ContentPage
     {
         InitializeComponent();
     }
+
+    private void btnApply_Clicked(object sender, EventArgs e)
+    {
+        this.txtDescription.Text = "New Description!";
+    }
 }
